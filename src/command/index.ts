97d8f6dca64789f83f525b5287994ae9164171d5
ChoiHyeongu <a11dsd123@gmail.com>
2022-06-18 15:33:35 +0900

    refact: 기존 Command를 handler로 교체하고 새로운 class 생성

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,1 +1,21 @@
-export * as command from './main';
+import { Message } from 'discord.js';
+import { config } from '../constants';
+class Command {
+  message: Message;
+  input: string;
+  args: any;
+
+  constructor(message: Message) {
+    this.message = message;
+    const [input, ...args] = this.parseMessage(message.content);
+    this.input = input;
+    this.args = args;
+  }
+
+  parseMessage(content: string): [string, string[]] {
+    const [input, ...args] = content.replace(config.PREFIX, '').split(' ');
+    return [input, args];
+  }
+}
+
+export default Command;
