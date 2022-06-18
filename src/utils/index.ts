7d72f8688c9196cc8fcf6f11766bca8989e060de
ChoiHyeongu <a11dsd123@gmail.com>
2022-06-18 15:33:15 +0900

    add: utils, isURL 추가

---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+/**
+ * 유요한 URL인지 확인
+ * @param str
+ * @returns boolean
+ */
+function isURL(str: string): boolean {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ); // fragment locator
+  return !!pattern.test(str);
+}
+
+export default { isURL };
